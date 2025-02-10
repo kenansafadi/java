9f41dd0bad74e0ae93f9e8fd361ad989ab894ac6
JSD <pro.mohammedghaben@gmail.com>
2025-02-10 22:06:30 +0200

    refactor: simplify main method in Q01.java for improved readability and functionality

--- q01.java
+++ q01.java
@@ -5,7 +5,6 @@ class Q01 {
 // 
 
     public static void main(String[] args) {
-
         // Perek A {START}
         // Node head = new Node(7);
         // head.setNext(new Node(21)).setNext(new Node(4));
@@ -19,16 +18,24 @@ class Q01 {
         // ----------------------------------------
         // Perek B {START}
         Random random = new Random(); // Create a Random object
-        Node<Integer> n = new Node(random.nextInt(99) + 2); // Get the number of nodes in the list
-        Node head2 = new Node(random.nextInt(99) + 2); // Create the head node with the random  number from 2 to 100
-        Node current2 = head2;  // Create the current node with the random number from
-        Node<Integer> i = new Node(0);
-        for (i = 0; i < n; i++) { //  Create the rest of the nodes with random numbers from 2 to 100
-            current2.setNext(new Node(random.nextInt(99) + 2)); //  Set the next node to the current node
-            current2 = current2.getNext();//  Set the current node to the next node
-            System.out.print(n.getData() + "~>");
-        }
-        // Perek B {END}
-    }
+        Node n = new Node(random.nextInt(99) + 2);  // Using Object as a generic type
+        Node head = new Node(1);
 
-}
+        Node current2 = head;  // Create the current node with the random number from
+        for (int i = 2; i <= n.getData(); i++) {
+            Node NewNode = new Node(i);
+            current2.setNext(NewNode);
+            current2 = NewNode;
+        }
+        System.out.println("Linked List:" + n.getData() + " ~>");
+        current2 = head;
+        while (current2 != null) {
+            System.out.print(current2.getData() + " ~> ");
+            current2 = current2.getNext();
+        }
+        System.out.println("Null");
+
+        // Perek B {END}
+    } // main 
+
+} //  Q01 class
