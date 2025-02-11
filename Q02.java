8f43fe465a00727b866dbe167b8808b7348bebd3
JSD <pro.mohammedghaben@gmail.com>
2025-02-11 20:20:17 +0200

    refactor: adjust formatting in Q02.java for improved readability

--- Q02.java
+++ Q02.java
@@ -1,7 +1,7 @@
 
 class Q02 {
 
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
 
         Node2<Object> head = new Node2<>(30);  // Using Object as a generic type
         head.setNext(new Node2<>("Hello")) // Adding a String
@@ -17,6 +17,5 @@ class Q02 {
                 System.out.println("null");
             }
         }
-
     }
 }
