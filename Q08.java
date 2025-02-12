a89594eeae5a5e3654524a681318f46984127f5d
JSD <pro.mohammedghaben@gmail.com>
2025-02-12 23:31:14 +0200

    Add compressSequences method and launch configuration for Java app

--- Q08.java
+++ Q08.java
@@ -1,6 +1,10 @@
 //Node ,Generic type
 public class Q08 {
+public static void compressSequences(Node<Character> chain){
 
+}
     public static void main(String[] args) {
+      Node<Character>head=new Node<>('k');
+      head.setNext(new Node<>('k')).setNext(new Node<>('k')).setNext(new Node<>('b'));
     }
 }
