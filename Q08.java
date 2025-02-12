64e359940ad431dc7a0987a3f0dd1b852bb7f460
JSD <pro.mohammedghaben@gmail.com>
2025-02-13 00:01:06 +0200

    Adding New Files,..1

--- Q08.java
+++ Q08.java
@@ -1,10 +1,12 @@
 //Node ,Generic type
 public class Q08 {
-public static void compressSequences(Node<Character> chain){
+    public static void compressSequences(Node<Character> chain) {
 
-}
+    }
+
     public static void main(String[] args) {
-      Node<Character>head=new Node<>('k');
-      head.setNext(new Node<>('k')).setNext(new Node<>('k')).setNext(new Node<>('b'));
+        Node<Character> head = new Node<>('k');
+        head.setNext(new Node<>('k')).setNext(new Node<>('k')).setNext(new Node<>('b')).setNext(new Node<>('b')).setNext(new Node<>('k')).setNext(new Node<>('z')).setNext(new Node<>('e')).setNext(new Node<>('e')).setNext(new Node<>('z'));
+        
     }
 }
