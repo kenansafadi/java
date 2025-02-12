93fdee76310965165ca545b50cc2950d9418e96a
JSD <pro.mohammedghaben@gmail.com>
2025-02-12 22:12:22 +0200

    Adding New Files,..1

--- Q04.java
+++ Q04.java
@@ -1,31 +1,38 @@
 import java.util.Random;
 
+
 public class Q04 {
+
     public static Node<Integer> createRandomChain(int numNodes) {
-//It's a Randomize Chain Node Numbers and  from 1 to numNodes.
         Random random = new Random();
-        Node<Integer> head = new Node<Integer>(0);
-        head.setNext(new Node<>(random.nextInt(99) + 1));
+        int nodeValue = random.nextInt(100) + 1; // Random number between 1-100
+        Node<Integer> head = new Node<>(nodeValue);
+        Node<Integer> current = head;
+        if (numNodes <= 0 || numNodes > 100) {
+            System.out.println("            Invalid number of nodes:\t ~>" + numNodes);
+            return null;
+        }
+        for (int i = 1; i < numNodes; i++) {  // Start from 1 because head is already created
+            nodeValue = random.nextInt(100) + 1;
+            Node<Integer> newNode = new Node<>(nodeValue);
+            current.next = newNode;  // Link the new node
+            current = newNode;  // Move to the new last node
+        }
+        return head;
+    }
+
+    // Function to print the linked list
+    public static void printList(Node<Integer> head) {
         Node<Integer> current = head;
         while (current != null) {
-        Node<Integer> NewNode = head.setNext(new Node<>(random.nextInt(99) + 1));
-            current.setNext(NewNode);
-
-
-            if (0 <= numNodes && numNodes <= 100) {
-                System.out.print("New Traverse Linked List Data: " + NewNode.getData() + " ~>");
-                return NewNode.getNext();
-            } else {
-                throw new IllegalStateException("Invalid Number of Node Number: " + current.getData());
-            }
-
-
+            System.out.print(current.getData() + " -> ");  // Print node value
+            current = current.getNext();  // Move to the next node
+            System.out.println("null");  // End of the list
         }
-        return null;
     }
 
     public static void main(String[] args) {
-        Q04 q04 = new Q04();
-        Node<Integer> randomChain = q04.createRandomChain(0);
+        Node<Integer> head = createRandomChain(-1);  // Create a linked list with 10 nodes
+        printList(head);  // Print the list
     }
 }
