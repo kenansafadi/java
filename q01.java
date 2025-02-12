eccad1b1957729863e11c14e0258ffe633aa957d
JSD <pro.mohammedghaben@gmail.com>
2025-02-12 21:35:28 +0200

    Adding New Files,..

--- q01.java
+++ q01.java
@@ -5,7 +5,7 @@ class Q01 {
     public static void main(String[] args) {
         // Perek A {START}
         // Node head = new Node(7);
-        // head.setNext(new Node(21)).setNext(new Node(4));
+        // head.setNext(new Node(21)).setNext(new Node(4));bvcxz
         // Node current = head;
         // while (current != null) {
         //     System.out.print(current.getData() + " => ");
