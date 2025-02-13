8da710318193d94b9f2bf15c7ac2d72c26e91c91
JSD <pro.mohammedghaben@gmail.com>
2025-02-13 23:16:43 +0200

    Adding New Files,..1

--- Q08.java
+++ Q08.java
@@ -4,26 +4,24 @@ public class Q08 {
         Node<Character> head = chain;
         Node<Character> current = head;
         current = current.getNext();
-        if(current.getData()==)
+        while (current.getNext() != null) {
+            if (current.getData() == current.getNext().getData()) {
+                Node<Character> newNode = current;
+                newNode.setData(head.getData());
+                current = newNode;
+                current = current.getNext();
+                System.out.print("compressed " + current.getData() + "~>");
+            }
+            current = current.getNext();
+        }
+
     }
 
     public static void main(String[] args) {
         Node<Character> head = new Node<>('k');
-        head.setNext(new Node<>('k'))
-                .setNext(new Node<>('k'))
-                .setNext(new Node<>('b')).setNext(new Node<>('b')).setNext(new Node<>('k'))
-                .setNext(new Node<>('z')).setNext(new Node<>('e'))
-                .setNext(new Node<>('e')).setNext(new Node<>('z'));
+        head.setNext(new Node<>('k')).setNext(new Node<>('k')).setNext(new Node<>('b')).setNext(new Node<>('b')).setNext(new Node<>('k')).setNext(new Node<>('z')).setNext(new Node<>('e')).setNext(new Node<>('e')).setNext(new Node<>('z'));
 
-        Node<Character> anotherHead = head;
-        anotherHead = anotherHead.getNext();
-        while (head.getNext() == anotherHead.getNext().getNext()) {
-
-            Node<Character> newNode = head;
-            while (newNode.getNext() != anotherHead.getNext()) {
-                newNode = newNode.getNext();
-            }
-        }
+        compressSequences(head);
 
 
     }
