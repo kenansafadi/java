02024652c51e70c6fe7b47877b9ab81659be2d3a
JSD <pro.mohammedghaben@gmail.com>
2025-02-20 20:08:54 +0200

    Add launch configurations for Q and Q01 classes and implement split_stack method in Q class

--- Exam_Questions/Q.java
+++ Exam_Questions/Q.java
@@ -1,13 +1,46 @@
+
 package Exam_Questions;
 
-public static void split_stack(Stack<Integer> s)
-{
-Stack<Integer> temps = new Stack<Integer>();
-Stack<Integer> t1 = new Stack<Integer>();
-
-
-
-}
+import java.util.*;
 public class Q {
+    public static void split_stack(Stack<Integer> s) {
+        Stack<Integer> temps = new Stack<Integer>();
+        Stack<Integer> t1 = new Stack<Integer>();
+        int cnt = 0;
+
+        while (!s.isEmpty()) {
+            if (cnt != 3) {
+                temps.push(s.pop());
+                cnt++;
+                t1.push(temps.pop());
+            }
+            System.out.println("TEMPS  ~> "+temps);
+            while (!temps.isEmpty() && cnt == 3) {
+                temps.pop();
+            }
+            cnt = 0;
+            while (temps.isEmpty() && cnt == 0) {
+                temps.push(s.pop());
+                t1.push(temps.pop());
+                cnt++;
+                System.out.println("T1 ~> "+t1);
+            }
+        }
+            
+    }
+public static void main(String[] args) {
+    Stack<Integer> s = new Stack<Integer>();
+    // insert 23,5,67,45,25,37,12
+    s.push(23);
+    s.push(5);
+    s.push(67);
+    s.push(45);
+    s.push(25);
+    s.push(37);
+    s.push(12);
+
+    split_stack(s);
+   
+}
     
 }
