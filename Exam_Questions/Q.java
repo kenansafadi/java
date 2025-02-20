6aab7811bfee88559b86b65dac15e83f98301de8
JSD <pro.mohammedghaben@gmail.com>
2025-02-20 20:21:16 +0200

    Add initial files for smart-buffer and node-gyp, and update lodash modules

--- Exam_Questions/Q.java
+++ Exam_Questions/Q.java
@@ -1,7 +1,7 @@
 
 package Exam_Questions;
 
-import java.util.*;
+import java.util.Stack;
 public class Q {
     public static void split_stack(Stack<Integer> s) {
         Stack<Integer> temps = new Stack<Integer>();
@@ -14,7 +14,6 @@ public class Q {
                 cnt++;
                 t1.push(temps.pop());
             }
-            System.out.println("TEMPS  ~> "+temps);
             while (!temps.isEmpty() && cnt == 3) {
                 temps.pop();
             }
@@ -30,12 +29,12 @@ public class Q {
     }
 public static void main(String[] args) {
     Stack<Integer> s = new Stack<Integer>();
-    // insert 23,5,67,45,25,37,12
+    // insert 23,5,67,45,24,37,12
     s.push(23);
     s.push(5);
     s.push(67);
     s.push(45);
-    s.push(25);
+    s.push(24);
     s.push(37);
     s.push(12);
 
@@ -44,3 +43,9 @@ public static void main(String[] args) {
 }
     
 }
+/*
+ * 
+ * t1= [12,37]
+ * t1=[12,37,25,45]
+ * t1=[12,37,25,45,67,5]
+ */
